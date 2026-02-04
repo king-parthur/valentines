30105ba85fa1af32976e12ca26b05205ac8cf598
king-parthur <parthivikapur@gmail.com>
2026-02-04 16:17:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "jhootha.",              // Shows when they go past 1000%
-        normal: "whatever, mwah i love u "                           // Shows when they go past 100%
+        extreme: "whatever, mwah i love u",  // Shows when they go past 2000%
+        high: "jhootha.",              // Shows when they go past 800%
+        normal: "U LOVE ME ITNA??"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
